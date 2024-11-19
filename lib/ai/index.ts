22933121e0109fda62d01a9336dd860841aa88a4
monkwhosoldpen <monkwhosoldpen@gmail.com>
2024-11-19 13:30:38 +0000

    test

--- lib/ai/index.ts
+++ lib/ai/index.ts
@@ -5,7 +5,7 @@ import { customMiddleware } from './custom-middleware';
 
 export const customModel = (apiIdentifier: string) => {
   return wrapLanguageModel({
-    model: groq('apiIdentifier'),
+    model: groq(apiIdentifier),
     middleware: customMiddleware,
   });
 };
