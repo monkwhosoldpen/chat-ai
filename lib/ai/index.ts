84a8f2b0e771268aed49d99c33dd9cb32ff5ecf3
monkwhosoldpen <monkwhosoldpen@gmail.com>
2024-11-19 13:10:05 +0000

    test

--- lib/ai/index.ts
+++ lib/ai/index.ts
@@ -1,11 +1,11 @@
 import { openai } from '@ai-sdk/openai';
 import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
-
+import { groq } from '@ai-sdk/groq';
 import { customMiddleware } from './custom-middleware';
 
 export const customModel = (apiIdentifier: string) => {
   return wrapLanguageModel({
-    model: openai(apiIdentifier),
+    model: groq(apiIdentifier),
     middleware: customMiddleware,
   });
 };
